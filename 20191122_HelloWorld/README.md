47561beae895ee252808b873d66a8fbab340931f
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2019-11-22 11:16:01 +0900

    Express 없이 http 모듈만으로 라우팅 처리해보기

--- 20191122_HelloWorld/README.md
+++ 20191122_HelloWorld/README.md
@@ -6,3 +6,6 @@
 - 기본 제공되는 http 모듈을 사용한다
 - createServer(callback())을 통해 실행하고자 하는 동작이 담긴 서버 객체를 만든다
 - 만들어진 서버 객체의 listen() 함수를 실행해서 API 서버를 띄워놓을 수 있다.
+- createServer()의 인자로 전달 된 콜백 함수 인자 중 req 인자를 이용해 라우팅이 가능하다.
+- req 객체 안에는 더럽게 많은 속성이 있다.
+- 정확하게 무엇이 오는지는 Node 문서 참고(https://nodejs.org/dist/latest-v12.x/docs/api/http.html)
--- 20191122_HelloWorld/index.js
+++ 20191122_HelloWorld/index.js
@@ -5,9 +5,22 @@ const port = 3000;
 
 // http.createServer()를 통해 생성 된 서버 객체를 반영한다
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end('Hello, World!\n');
+
+  // request 객체에 따른 라우팅 분기
+  if (req.url === '/') {
+    // 전달할 response 객체 생성
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end('Hello, World!\n');
+  } else if (req.url === '/users') {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'application/json');
+    res.end('User list\n');
+  }
+  else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
 });
 
 // 지정 된 포트, 호스트에서 하루좽일 위의 생성된 서버를 실행한다.
